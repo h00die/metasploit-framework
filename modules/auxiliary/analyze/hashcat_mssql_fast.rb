fc1671ff043be5798234b9916a3432466cef7a30
h00die <michaeltcyr@gmail.com>
2019-02-11 16:49:12 -0500

    more workingish hashcat

--- modules/auxiliary/analyze/hashcat_mssql_fast.rb
+++ modules/auxiliary/analyze/hashcat_mssql_fast.rb
@@ -13,34 +13,18 @@ class MetasploitModule < Msf::Auxiliary
       'Name'            => 'Hashcat Linux Password Cracker',
       'Description'     => %Q{
           This module uses Hashcat to identify weak passwords that have been
-        acquired from unshadowed passwd files from Unix systems. The module will only crack
-        MD5, BSDi and DES implementations by default. Set Crypt to true to also try to crack
-        Blowfish and SHA(256/512). Warning: This is much slower.
-        MD5 is format 500 in hashcat.
-        BSDi is format 12400 in hashcat.
-        DES is format 14000 in hashcat.
-        Blowfish/bf/bcrypt is format 3200 in hashcat.
-        Sha256 is format 7400 in hashcat.
-        Sha512 is format 1800 in hashcat.
+        acquired from the mssql_hashdump module. 
+        mssql is format 131 in hashcat.
+        mssql05 is format 132 in hashcat.
+        mssql12 is format 1731 in hashcat.
       },
       'Author'          => ['h00die'],
       'License'         => MSF_LICENSE
     )
-    register_options(
-      [
-        OptBool.new('Crypt',[false, 'Try blowfish/sha256/sha512 format hashes(Very Slow)', false])
-      ]
-    )
   end
 
   def run
-
-    formats = [ 'md5crypt', 'descrypt', 'bsdicrypt']
-    if datastore['Crypt']
-      formats << 'sha256crypt'
-      formats << 'sha512crypt'
-      formats << 'bcrypt'
-    end
+    @formats = Set.new
 
     cracker = new_hashcat_cracker
 
@@ -50,7 +34,7 @@ class MetasploitModule < Msf::Auxiliary
     cracker.hash_path, hashes = hash_file
 
     # generate our wordlist and close the file handle.  max length of DES is 8
-    wordlist = wordlist_file(8)
+    wordlist = wordlist_file
     unless wordlist
       print_error('This module cannot run without a database connected. Use db_connect to connect to a database.')
       return
@@ -62,7 +46,7 @@ class MetasploitModule < Msf::Auxiliary
 
     cleanup_files = [cracker.hash_path, wordlist.path]
 
-    formats.each do |format|
+    @formats.each do |format|
       # dupe our original cracker so we can safely change options between each run
       cracker_instance = cracker.dup
       cracker_instance.format = jtr_format_to_hashcat_format(format)
@@ -99,26 +83,21 @@ class MetasploitModule < Msf::Auxiliary
       end
     end
     cleanup_files.each do |f|
-      File.delete(f)
+      #File.delete(f)
     end
   end
 
   def hash_file
     hashes = []
-    wrote_hash = false
     hashlist = Rex::Quickfile.new("hashes_tmp")
     framework.db.creds(workspace: myworkspace, type: 'Metasploit::Credential::NonreplayableHash').each do |core|
-      if core.private.jtr_format =~ /md5|des|bsdi|crypt|bf|sha256|sha512|sha-256|sha-512/
-        hashes << "#{core.private.data}:#{core.public.username}:#{core.id}"
+      if core.private.jtr_format =~ /mssql|mssql05|mssql12/
+        @formats << core.private.jtr_format
         hashlist.puts hash_to_hashcat(core)
-        wrote_hash = true
+        hashes << "#{core.private.data}:#{core.public.username}:#{core.id}"
       end
     end
     hashlist.close
-    unless wrote_hash # check if we wrote anything and bail early if we didn't
-      hashlist.delete
-      fail_with Failure::NotFound, 'No applicable hashes in database to crack'
-    end
     print_status "Hashes Written out to #{hashlist.path}"
     return hashlist.path, hashes
   end
